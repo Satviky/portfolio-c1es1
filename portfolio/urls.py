695a35c0662f39a11a314df908646f48f1ff61ca
Satviky <126392608+Satviky@users.noreply.github.com>
2025-02-25 12:55:37 +0530

    fixing

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from portfolio import views
 
 urlpatterns = [
