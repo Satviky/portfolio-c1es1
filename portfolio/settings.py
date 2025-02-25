c517cdecfa03ab90e325f5bcde78dc080a7e444f
Satviky <126392608+Satviky@users.noreply.github.com>
2025-02-25 14:47:36 +0530

    fixing rendering issue #  1

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -116,13 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATIC_ROOT = "/var/www/example.com/static/"
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,11 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from portfolio import views
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 
-from portfolio import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -32,4 +33,6 @@ urlpatterns = [
     path('contact/',views.contact),
     path('projects/',views.projects),
     path('info',views.inf),
-]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]  
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
